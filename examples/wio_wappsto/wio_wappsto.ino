c97d0c2a8b8d3f0aea7d186d95138bfd422e5be9
Karsten Tonnesen <karsten@seluxit.com>
2021-11-30 13:39:58 +0100

    Moved ntp code to seperate file.

--- examples/wio_wappsto/wio_wappsto.ino
+++ examples/wio_wappsto/wio_wappsto.ino
@@ -1,5 +1,5 @@
 #include <ArduinoJson.h>
-#include "rpcWiFi.h" // Seeed arduino rpc unified, seeed arduino rpcwifi, seeed arduino rpcble
+#include "rpcWiFi.h"
 #include <WiFiMulti.h>
 #include <WiFiClientSecure.h>
 
@@ -22,6 +22,7 @@ WiFiUDP udp;
 unsigned long devicetime;
 RTC_SAMD51 rtc;
 
+static char strfTimeBuffer[21];
 
 const char* ssid = "";
 const char* password = "";
@@ -32,13 +33,14 @@ const char* ca = "-----BEGIN CERTIFICATE-----\n    \n-----END CERTIFICATE-----\n
 const char* client_crt = "-----BEGIN CERTIFICATE-----\n  \n-----END CERTIFICATE-----\n";
 const char* client_key = "-----BEGIN RSA PRIVATE KEY-----\n  \n-----END RSA PRIVATE KEY-----\n";
 
+
 Device *myDevice;
 Value *myNumberValue;
 Value *myStringValue;
 ValueNumber_t myNumberValueParameters;
 ValueString_t myStringValueParameters;
 
-int counter = 0;
+int myNumberCounter = 0;
 
 #define UUID_COUNT 7
 uint32_t deviceIndex[] = {
@@ -75,7 +77,6 @@ const char* readUuid(uint32_t storeId)
     return NULL;
 }
 
-static char strfTimeBuffer[21];
 
 const char* getUtcTime(void)
 {
@@ -92,16 +93,16 @@ void controlNumberCallback(void *object, String data)
     Value *val = (Value*)object;
     Serial.print("Control callback: ");
     Serial.println(data);
-    counter++;
-    val->report(String(counter));
+    myNumberCounter++;
+    val->report(String(myNumberCounter));
 }
 
 void refreshNumberCallback(void *object, String data)
 {
     Value *val = (Value*)object;
     Serial.print("Refresh callback: ");
-    counter++;
-    val->report(String(counter));
+    myNumberCounter++;
+    val->report(String(myNumberCounter));
 }
 
 void controlStringCallback(void *object, String data)
@@ -131,107 +132,7 @@ void initializeWifi(void)
     Serial.println(WiFi.localIP());
 }
 
-unsigned long getNTPtime()
-{
-    // module returns a unsigned long time valus as secs since Jan 1, 1970
-    // unix time or 0 if a problem encounted
 
-    //only send data when connected
-    if (WiFi.status() == WL_CONNECTED) {
-        //initializes the UDP state
-        //This initializes the transfer buffer
-        udp.begin(WiFi.localIP(), localPort);
-
-        sendNTPpacket(timeServer); // send an NTP packet to a time server
-        // wait to see if a reply is available
-        delay(1000);
-
-        if (udp.parsePacket()) {
-            // We've received a packet, read the data from it
-            udp.read(packetBuffer, NTP_PACKET_SIZE); // read the packet into the buffer
-
-            //the timestamp starts at byte 40 of the received packet and is four bytes,
-            // or two words, long. First, extract the two words:
-
-            unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
-            unsigned long lowWord = word(packetBuffer[42], packetBuffer[43]);
-            // combine the four bytes (two words) into a long integer
-            // this is NTP time (seconds since Jan 1 1900):
-            unsigned long secsSince1900 = highWord << 16 | lowWord;
-            // Unix time starts on Jan 1 1970. In seconds, that's 2208988800:
-            const unsigned long seventyYears = 2208988800UL;
-            // subtract seventy years:
-            unsigned long epoch = secsSince1900 - seventyYears;
-
-            return epoch;
-        }
-        else {
-            // were not able to parse the udp packet successfully
-            // clear down the udp connection
-            udp.stop();
-            return 0; // zero indicates a failure
-        }
-        // not calling ntp time frequently, stop releases resources
-        udp.stop();
-    }
-    else {
-        // network not connected
-        return 0;
-    }
-}
-
-
- // send an NTP request to the time server at the given address
-unsigned long sendNTPpacket(const char* address)
-{
-    // set all bytes in the buffer to 0
-    for (int i = 0; i < NTP_PACKET_SIZE; ++i) {
-        packetBuffer[i] = 0;
-    }
-    // Initialize values needed to form NTP request
-    // (see URL above for details on the packets)
-    packetBuffer[0] = 0b11100011;   // LI, Version, Mode
-    packetBuffer[1] = 0;     // Stratum, or type of clock
-    packetBuffer[2] = 6;     // Polling Interval
-    packetBuffer[3] = 0xEC;  // Peer Clock Precision
-    // 8 bytes of zero for Root Delay & Root Dispersion
-    packetBuffer[12] = 49;
-    packetBuffer[13] = 0x4E;
-    packetBuffer[14] = 49;
-    packetBuffer[15] = 52;
-
-    // all NTP fields have been given values, now
-    // you can send a packet requesting a timestamp:
-    udp.beginPacket(address, 123); //NTP requests are to port 123
-    udp.write(packetBuffer, NTP_PACKET_SIZE);
-    udp.endPacket();
-}
-
-void initializeNtp(void)
-{
-    // get the time via NTP (udp) call to time server
-    // getNTPtime returns epoch UTC time adjusted for timezone but not daylight savings
-    // time
-    devicetime = getNTPtime();
-
-    // check if rtc present
-    if (devicetime == 0) {
-        Serial.println("Failed to get time from network time server.");
-    }
-
-    if (!rtc.begin()) {
-        Serial.println("Couldn't find RTC");
-        while (1) delay(10); // stop operating
-    }
-
-    // adjust time using ntp time
-    rtc.adjust(DateTime(devicetime));
-
-    // get and print the adjusted rtc time
-    now = rtc.now();
-    Serial.print("UTC (boot) time is: ");
-    Serial.println(now.timestamp(DateTime::TIMESTAMP_FULL));
-}
 
 void setup() {
     Serial.begin(115200);
@@ -273,6 +174,6 @@ void loop() {
     delay(500);
     wappsto.dataAvailable();
 
-    //myNumberValue->report(String(counter));
-    //counter++;
+    //myNumberValue->report(String(myNumberCounter));
+    //myNumberCounter++;
 }
