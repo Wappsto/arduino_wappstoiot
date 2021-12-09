d6d985d0948ddfb20c21dafba57ffc74d29f08f5
Karsten Tonnesen <karsten@seluxit.com>
2021-12-09 17:03:09 +0100

    Added example demo app using wio hw features.

---
+++ examples/wio_tft_wappsto/ntp.ino
@@ -0,0 +1,122 @@
+
+static char strfTimeBuffer[21];
+
+const char* getUtcTime(void)
+{
+    now = rtc.now();
+    time_t t = now.unixtime();
+    struct tm *timeinfo = localtime(&t);
+    strftime(strfTimeBuffer, sizeof(strfTimeBuffer), "%FT%TZ", timeinfo);
+    Serial.println(strfTimeBuffer);
+    return strfTimeBuffer;
+}
+
+unsigned long getNTPtime()
+{
+    // module returns a unsigned long time valus as secs since Jan 1, 1970
+    // unix time or 0 if a problem encounted
+
+    //only send data when connected
+    if (WiFi.status() == WL_CONNECTED) {
+        //initializes the UDP state
+        //This initializes the transfer buffer
+        udp.begin(WiFi.localIP(), localPort);
+
+        sendNTPpacket(timeServer); // send an NTP packet to a time server
+        // wait to see if a reply is available
+        delay(1000);
+
+        if (udp.parsePacket()) {
+            // We've received a packet, read the data from it
+            udp.read(packetBuffer, NTP_PACKET_SIZE); // read the packet into the buffer
+
+            //the timestamp starts at byte 40 of the received packet and is four bytes,
+            // or two words, long. First, extract the two words:
+
+            unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
+            unsigned long lowWord = word(packetBuffer[42], packetBuffer[43]);
+            // combine the four bytes (two words) into a long integer
+            // this is NTP time (seconds since Jan 1 1900):
+            unsigned long secsSince1900 = highWord << 16 | lowWord;
+            // Unix time starts on Jan 1 1970. In seconds, that's 2208988800:
+            const unsigned long seventyYears = 2208988800UL;
+            // subtract seventy years:
+            unsigned long epoch = secsSince1900 - seventyYears;
+
+            return epoch;
+        }
+        else {
+            // were not able to parse the udp packet successfully
+            // clear down the udp connection
+            udp.stop();
+            return 0; // zero indicates a failure
+        }
+        // not calling ntp time frequently, stop releases resources
+        udp.stop();
+    }
+    else {
+        // network not connected
+        return 0;
+    }
+}
+
+
+ // send an NTP request to the time server at the given address
+unsigned long sendNTPpacket(const char* address)
+{
+    // set all bytes in the buffer to 0
+    for (int i = 0; i < NTP_PACKET_SIZE; ++i) {
+        packetBuffer[i] = 0;
+    }
+    // Initialize values needed to form NTP request
+    // (see URL above for details on the packets)
+    packetBuffer[0] = 0b11100011;   // LI, Version, Mode
+    packetBuffer[1] = 0;     // Stratum, or type of clock
+    packetBuffer[2] = 6;     // Polling Interval
+    packetBuffer[3] = 0xEC;  // Peer Clock Precision
+    // 8 bytes of zero for Root Delay & Root Dispersion
+    packetBuffer[12] = 49;
+    packetBuffer[13] = 0x4E;
+    packetBuffer[14] = 49;
+    packetBuffer[15] = 52;
+
+    // all NTP fields have been given values, now
+    // you can send a packet requesting a timestamp:
+    udp.beginPacket(address, 123); //NTP requests are to port 123
+    udp.write(packetBuffer, NTP_PACKET_SIZE);
+    udp.endPacket();
+}
+
+void initializeNtp(void)
+{
+    uint8_t attempts = 3;
+    // get the time via NTP (udp) call to time server
+    // getNTPtime returns epoch UTC time adjusted for timezone but not daylight savings
+    // time
+
+    do {
+        devicetime = getNTPtime();
+        if(devicetime != 0) {
+            break;
+        }
+        attempts--;
+    } while(attempts > 0);
+
+    // check if rtc present
+    if (devicetime == 0) {
+        Serial.println("Failed to get time from network time server.");
+    }
+
+    if (!rtc.begin()) {
+        Serial.println("Couldn't find RTC");
+        while (1) delay(10); // stop operating
+    }
+
+    // adjust time using ntp time
+    rtc.adjust(DateTime(devicetime));
+
+    // get and print the adjusted rtc time
+    now = rtc.now();
+    Serial.print("UTC (boot) time is: ");
+    Serial.println(now.timestamp(DateTime::TIMESTAMP_FULL));
+}
---
+++ examples/wio_tft_wappsto/wio_wappsto.ino
@@ -0,0 +1,219 @@
+/** Getting started with Wio terminal
+Important step for new hardware - wifi firmware upgrade:
+https://wiki.seeedstudio.com/Wio-Terminal-Network-Overview/
+
+
+https://wiki.seeedstudio.com/Wio-Terminal-Getting-Started/
+- Add to preferences, additional boards manager urls: https://files.seeedstudio.com/arduino/package_seeeduino_boards_index.json
+- Board manager, search for wio, install Seeed SAMD Boards
+
+Libraries
+Seeed_Arduino_RTC
+Seeed_Arduino_rpcWiFi - search for "seeed rpcwifi"
+Seeed_Arduino_rpcUnified - search for "seeed rpcunified"
+Seeed_Arduino_mbedtls - search for "seeed mbedtls"
+Seeed_Arduino_FS - search for "seeed fs"
+Seeed_Arduino_SFUD - search for "seeed sfud"
+
+WappstoIoT Libraries requirements
+ArduinoJson
+WiFiClientSecure
+**/
+
+#include "rpcWiFi.h"
+#include <WiFiMulti.h>
+#include <WiFiClientSecure.h>
+
+#include "RTC_SAMD51.h" // arduino lib: Seeed_Arduino_RTC
+#include "DateTime.h"
+
+#include"TFT_eSPI.h"
+TFT_eSPI tft;
+
+#include "Wappsto.h"
+
+#define BUZZER_PIN WIO_BUZZER
+
+WiFiMulti WiFiMulti;
+WiFiClientSecure client;
+Wappsto wappsto(&client);
+
+unsigned int localPort = 2390;      // local port to listen for UDP packets
+char timeServer[] = "time.nist.gov"; // extenral NTP server e.g. time.nist.gov
+const int NTP_PACKET_SIZE = 48; // NTP time stamp is in the first 48 bytes of the message
+byte packetBuffer[NTP_PACKET_SIZE]; //buffer to hold incoming and outgoing packets
+// declare a time object
+DateTime now;
+WiFiUDP udp;
+unsigned long devicetime;
+RTC_SAMD51 rtc;
+
+const char* ssid = "";
+const char* password = "";
+
+const char* network_id = "ae9f973e-22b9-4c2a-9de5-b9eb5db64c3d";
+
+const char* ca = "-----BEGIN CERTIFICATE-----\n    \n-----END CERTIFICATE-----\n";
+const char* client_crt = "-----BEGIN CERTIFICATE-----\n  \n-----END CERTIFICATE-----\n";
+const char* client_key = "-----BEGIN RSA PRIVATE KEY-----\n  \n-----END RSA PRIVATE KEY-----\n";
+
+Network *myNetwork;
+Device *myDevice;
+Value *myNumberValue;
+Value *displayStrValue;
+Value *buttonValue;
+
+ValueNumber_t myNumberValueParameters = {.min = 100, .max = 4000, .step = 1, .unit = "", .si_conversion = ""};
+ValueString_t displayStrValueParameters = {.max = 200, .encoding = ""};
+ValueString_t buttonStringParameters = {.max = 6, .encoding = ""};
+
+
+void controlNumberCallback(Value *value, String data, String timestamp)
+{
+    int tone = data.toInt();
+    playTone(tone, 1000);
+    value->report(data);
+}
+
+void refreshNumberCallback(Value *value)
+{
+    Serial.print("Refresh callback: ");
+    value->report(value->getReportData());
+}
+
+void controlStringCallback(Value *value, String data, String timestamp)
+{
+    tft.fillScreen(TFT_GREEN);
+    tft.drawString(data, 10, 10);
+    value->report(data);
+}
+
+void refreshStringCallback(Value *value)
+{
+    tft.fillScreen(TFT_GREEN);
+    tft.drawString("Value was refreshed", 10, 10);
+    value->report(String("Refresh"));
+}
+
+void buttonRefresh(Value *value)
+{
+    value->report(String(""));
+}
+
+void deleteNetworkCallback(Network *network)
+{
+    Serial.println("Network deleted, need to restart device, and claim network again");
+}
+
+void initializeWifi(void)
+{
+    Serial.println("Initializing WiFi");
+    WiFiMulti.addAP(ssid, password);
+    while(WiFiMulti.run() != WL_CONNECTED) {
+        Serial.print(".");
+        delay(500);
+    }
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+}
+
+void playTone(int tone, int duration)
+{
+    for (long i = 0; i < duration * 1000L; i += tone * 2) {
+        digitalWrite(BUZZER_PIN, HIGH);
+        delayMicroseconds(tone);
+        digitalWrite(BUZZER_PIN, LOW);
+        delayMicroseconds(tone);
+    }
+}
+
+void initialiseWioOutput(void)
+{
+    pinMode(WIO_5S_UP, INPUT_PULLUP);
+    pinMode(WIO_5S_DOWN, INPUT_PULLUP);
+    pinMode(WIO_5S_LEFT, INPUT_PULLUP);
+    pinMode(WIO_5S_RIGHT, INPUT_PULLUP);
+    pinMode(WIO_5S_PRESS, INPUT_PULLUP);
+    pinMode(BUZZER_PIN, OUTPUT);
+}
+
+void setup()
+{
+    Serial.begin(115200);
+
+    // Screen update
+    tft.begin();
+    tft.setRotation(3);
+    tft.setTextColor(TFT_BLACK);
+    tft.setTextSize(2);
+    tft.fillScreen(TFT_RED);
+
+    randomSeed(analogRead(0));
+    initialiseWioOutput();
+
+    // Screen update
+    tft.drawString("Connecting to WiFi", 10, 10);
+
+    initializeWifi();
+
+    // Screen update
+    tft.drawString(WiFi.localIP().toString(), 10, 40);
+    tft.drawString("Getting NTP", 10, 70);
+
+    initializeNtp();
+
+    // Screen update
+    tft.fillScreen(TFT_YELLOW);
+    tft.drawString("Connecting to Wappsto", 10, 10);
+
+    if(wappsto.connect(network_id, ca, client_crt, client_key)) {
+        Serial.println("Connected to Wappsto");
+    } else {
+        Serial.println("Could not connect");
+        // Screen update
+        tft.fillScreen(TFT_RED);
+        tft.drawString("Could not connect to Wappsto", 10, 10);
+    }
+
+    // Create network
+    myNetwork = wappsto.createNetwork("Demo");
+
+    // Create device
+    myDevice = myNetwork->createDevice("My Demo Device", "", "", "", "", "");
+
+    // Create r/w number to play tone
+    myNumberValue = myDevice->createValueNumber("Beep number", "audio number", READ_WRITE, &myNumberValueParameters);
+    myNumberValue->onControl(&controlNumberCallback);
+    myNumberValue->onRefresh(&refreshNumberCallback);
+
+    // Create r/w value for display string
+    displayStrValue = myDevice->createValueString("Display Sting", "string", READ_WRITE, &displayStrValueParameters);
+    displayStrValue->onControl(&controlStringCallback);
+    displayStrValue->onRefresh(&refreshStringCallback);
+
+    // Create r value for button output
+    buttonValue = myDevice->createValueString("Button direction", "string", READ, &buttonStringParameters);
+    buttonValue->onRefresh(&buttonRefresh);
+
+    // Screen update
+    tft.fillScreen(TFT_GREEN);
+    tft.drawString("Connected", 10, 10);
+}
+
+void loop()
+{
+    wappsto.dataAvailable(); // Required to receive data from Wappsto
+
+    if (digitalRead(WIO_5S_UP) == LOW) {
+        buttonValue->report("Up");
+    } else if (digitalRead(WIO_5S_DOWN) == LOW) {
+        buttonValue->report("Down");
+    } else if (digitalRead(WIO_5S_LEFT) == LOW) {
+        buttonValue->report("Left");
+    } else if (digitalRead(WIO_5S_RIGHT) == LOW) {
+        buttonValue->report("Right");
+    } else if (digitalRead(WIO_5S_PRESS) == LOW) {
+        buttonValue->report("Press");
+    }
+    delay(200);
+}
