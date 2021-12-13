89c9eac9a96a4b255b0fe2ac73282b650daa7850
Karsten Tonnesen <karsten@seluxit.com>
2021-12-13 15:29:09 +0100

    Added Esp32 temperature example.

---
+++ examples/esp32_wappsto/esp32_wappsto.ino
@@ -0,0 +1,97 @@
+/** Getting started with ESP32
+
+https://randomnerdtutorials.com/installing-the-esp32-board-in-arduino-ide-windows-instructions/
+- Add to preferences, additional boards manager urls: https://dl.espressif.com/dl/package_esp32_index.json
+- Board manager, search for ESP32 and install
+
+WappstoIoT Libraries requirements
+ArduinoJson
+WiFiClientSecure
+**/
+
+#include <WiFiMulti.h>
+#include <WiFiClientSecure.h>
+
+#include "Wappsto.h"
+#include "wappsto_setup.h"
+
+WiFiMulti WiFiMulti;
+WiFiClientSecure client;
+Wappsto wappsto(&client);
+
+const char* ssid = "";
+const char* password = "";
+
+Network *myNetwork;
+Device *myDevice;
+Value *myTemperatureValue;
+
+DeviceDescription_t myDeviceDescription = {
+    .product = "ESP32 example",
+    .manufacturer = "",
+    .description = "Example description",
+    .version = "1.0",
+    .serial = "00001",
+    .protocol = "Json-RPC",
+    .communication = "WiFi",
+};
+ValueNumber_t myTemperatureParameters = {.min = -20, .max = 100, .step = 0.1, .unit = "°C", .si_conversion = ""};
+
+double myTemperatureReading = 21.3;
+
+void refreshTemperatureCallback(Value *value)
+{
+    value->report(myTemperatureReading);
+}
+
+void initializeWifi(void)
+{
+    Serial.println("Initializing WiFi");
+    WiFiMulti.addAP(ssid, password);
+    while(WiFiMulti.run() != WL_CONNECTED) {
+        Serial.print(".");
+        delay(500);
+    }
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+}
+
+void setup() {
+    Serial.begin(115200);
+    randomSeed(analogRead(0));
+    initializeWifi();
+
+    initializeNtp();
+
+    wappsto.config(network_id, ca, client_crt, client_key);
+    if(wappsto.connect()) {
+        Serial.println("Connected to Wappsto");
+    } else {
+        Serial.println("Could not connect");
+    }
+
+    // Create network
+    myNetwork = wappsto.createNetwork("Basic Example");
+
+    // Create device
+    myDevice = myNetwork->createDevice("My Device", &myDeviceDescription);
+
+    // Create temperature value
+    myTemperatureValue = myDevice->createValueNumber("Temperature", "temperature", READ, &myTemperatureParameters);
+    myTemperatureValue->onRefresh(&refreshTemperatureCallback);
+}
+
+unsigned long startMillis = 0;
+unsigned long currentMillis = 0;
+#define UPDATE_INTERVAL_MILLIS 2*60*1000
+
+void loop() {
+    delay(500);
+    wappsto.dataAvailable();
+
+    currentMillis = millis();
+    if(currentMillis - startMillis >= UPDATE_INTERVAL_MILLIS) {
+        startMillis = currentMillis;
+        myTemperatureValue->report(myTemperatureReading);
+    }
+}
---
+++ examples/esp32_wappsto/wappsto_setup.h
@@ -0,0 +1,5 @@
+const char* network_id = "...";
+
+const char* ca = "-----BEGIN CERTIFICATE-----\n    \n-----END CERTIFICATE-----\n";
+const char* client_crt = "-----BEGIN CERTIFICATE-----\n  \n-----END CERTIFICATE-----\n";
+const char* client_key = "-----BEGIN RSA PRIVATE KEY-----\n  \n-----END RSA PRIVATE KEY-----\n";
