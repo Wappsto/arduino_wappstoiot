0bdcad8d857d3f0698418da1ecf684daba4c25c9
Karsten Tonnesen <karsten@seluxit.com>
2021-12-14 14:50:49 +0100

    Use arduino random instead of esp_random.

--- src/WappstoIoT.cpp
+++ src/WappstoIoT.cpp
@@ -1,25 +1,17 @@
-
+#include "Arduino.h"
 #include "WappstoIoT.h"
 
 void generateNewUuid(char *str)
 {
-#if defined(ESP32)
-    sprintf(str, "%08x-%04x-%04x-%04x-%04x%08x",
-            esp_random(),
-            esp_random() & 0xFFFF,
-            esp_random() & 0xFFFF,
-            esp_random() & 0xBFFF, // UUID variant = 8 .. b
-            esp_random() & 0xFFFF,
-            esp_random());
-#else
-    sprintf(str, "%08x-%04x-%04x-%04x-%04x%08x",
-            random(),
-            random() & 0xFFFF,
-            random() & 0xFFFF,
-            random() & 0xBFFF, // UUID variant = 8 .. b
-            random() & 0xFFFF,
-            random());
-#endif
+    sprintf(str, "%04x%04x-%04x-%04x-%04x-%04x%04x%04x",
+            random(0xFFFF) & 0xFFFF,
+            random(0xFFFF) & 0xFFFF,
+            random(0xFFFF) & 0xFFFF,
+            random(0xFFFF) & 0xFFFF,
+            random(0xFFFF) & 0xBFFF, // UUID variant = 8 .. b
+            random(0xFFFF) & 0xFFFF,
+            random(0xFFFF) & 0xFFFF,
+            random(0xFFFF) & 0xFFFF);
     // UUID verion 4 = random
     str[14] = '4';
     str[36] = '\0';
