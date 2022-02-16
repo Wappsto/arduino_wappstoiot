fe6a0ab0b0249a0f3d30c3752714574d024b88c1
Karsten Tonnesen <karsten@seluxit.com>
2022-02-16 14:23:23 +0100

    Added value types to test case.

--- tests/CreateTest/CreateTest.ino
+++ tests/CreateTest/CreateTest.ino
@@ -6,15 +6,168 @@ test(createNetwork) {
     WiFiClientSecure client;
 
     Wappsto wappsto(&client);
-    wappsto.config("4906c6be-cc7f-4c4d-8806-60a38c5fcef5", "", "", "", 0, VERBOSE);
+    wappsto.config("4906c6be-cc7f-4c4d-8806-60a38c5fcef5", "", "", "", 0, NO_LOGS);
 
     wappsto.connect();
 
     Network *myNetwork = wappsto.createNetwork("Basic Example", "");
     assertNotEqual(myNetwork, NULL);
     assertEqual(myNetwork->name, "Basic Example");
-    int len = 36; //strlen(myNetwork->uuid);
-    assertEqual(len, 36);
+}
+
+test(createDevice) {
+    WiFiClientSecure client;
+
+    // Test setup
+    client.addDeviceUuid("42b7bb41-bf32-4648-1102-aea6fca55641");
+
+    Wappsto wappsto(&client);
+    wappsto.config("4906c6be-cc7f-4c4d-8806-60a38c5fcef5", "", "", "", 0, NO_LOGS);
+
+    wappsto.connect();
+
+    Network *myNetwork = wappsto.createNetwork("Basic Example", "");
+    assertNotEqual(myNetwork, NULL);
+
+    DeviceDescription_t myDeviceDescription = {
+        .name = "My Test Device",
+        .product = "CreateTest",
+        .manufacturer = "",
+        .description = "description",
+        .version = "1.0",
+        .serial = "00001",
+        .protocol = "Json-RPC",
+        .communication = "WiFi",
+    };
+    Device *myDevice = myNetwork->createDevice(&myDeviceDescription);
+    assertNotEqual(myDevice, NULL);
+
+}
+
+test(createValueNumber) {
+    WiFiClientSecure client;
+
+    // Test setup
+    client.addDeviceUuid("42b7bb41-bf32-4648-1102-aea6fca55641");
+    client.addValueUuid("42b7bb41-bf32-4648-1102-aea6fca55642");
+    client.addReportUuid("42b7bb41-bf32-4648-1102-aea6fca55643", "0");
+    client.addControlUuid("42b7bb41-bf32-4648-1102-aea6fca55644", "0");
+
+    Wappsto wappsto(&client);
+    wappsto.config("4906c6be-cc7f-4c4d-8806-60a38c5fcef5", "", "", "", 0, NO_LOGS);
+
+    wappsto.connect();
+
+    Network *myNetwork = wappsto.createNetwork("Basic Example", "");
+    assertNotEqual(myNetwork, NULL);
+
+    DeviceDescription_t myDeviceDescription = {
+        .name = "My Test Device",
+        .product = "CreateTest",
+        .manufacturer = "",
+        .description = "description",
+        .version = "1.0",
+        .serial = "00001",
+        .protocol = "Json-RPC",
+        .communication = "WiFi",
+    };
+    Device *myDevice = myNetwork->createDevice(&myDeviceDescription);
+    assertNotEqual(myDevice, NULL);
+
+    ValueNumber_t numberParams = {  .name = "Test number",
+                                    .type = "test type",
+                                    .permission = READ_WRITE,
+                                    .min = 0,
+                                    .max = 1,
+                                    .step = 1,
+                                    .unit = "",
+                                    .si_conversion = ""};
+
+
+    Value *myNumberValue = myDevice->createValueNumber(&numberParams);
+    assertNotEqual(myNumberValue, NULL);
+}
+
+test(createValueString) {
+    WiFiClientSecure client;
+
+    // Test setup
+    client.addDeviceUuid("42b7bb41-bf32-4648-1102-aea6fca55641");
+    client.addValueUuid("42b7bb41-bf32-4648-1102-aea6fca55642");
+    client.addReportUuid("42b7bb41-bf32-4648-1102-aea6fca55643", "Test data");
+    client.addControlUuid("42b7bb41-bf32-4648-1102-aea6fca55644", "Test data");
+
+    Wappsto wappsto(&client);
+    wappsto.config("4906c6be-cc7f-4c4d-8806-60a38c5fcef5", "", "", "", 0, NO_LOGS);
+
+    wappsto.connect();
+
+    Network *myNetwork = wappsto.createNetwork("Basic Example", "");
+    assertNotEqual(myNetwork, NULL);
+
+    DeviceDescription_t myDeviceDescription = {
+        .name = "My Test Device",
+        .product = "CreateTest",
+        .manufacturer = "",
+        .description = "description",
+        .version = "1.0",
+        .serial = "00001",
+        .protocol = "Json-RPC",
+        .communication = "WiFi",
+    };
+    Device *myDevice = myNetwork->createDevice(&myDeviceDescription);
+    assertNotEqual(myDevice, NULL);
+
+    ValueString_t stringParams = {  .name = "Test String",
+                                    .type = "test type",
+                                    .permission = READ_WRITE,
+                                    .max = 25,
+                                    .encoding = ""};
+
+
+    Value *myStringValue = myDevice->createValueString(&stringParams);
+    assertNotEqual(myStringValue, NULL);
+}
+
+test(createValueBlob) {
+    WiFiClientSecure client;
+
+    // Test setup
+    client.addDeviceUuid("42b7bb41-bf32-4648-1102-aea6fca55641");
+    client.addValueUuid("42b7bb41-bf32-4648-1102-aea6fca55642");
+    client.addReportUuid("42b7bb41-bf32-4648-1102-aea6fca55643", "04FFEA");
+    client.addControlUuid("42b7bb41-bf32-4648-1102-aea6fca55644", "04FFEA");
+
+    Wappsto wappsto(&client);
+    wappsto.config("4906c6be-cc7f-4c4d-8806-60a38c5fcef5", "", "", "", 0, NO_LOGS);
+
+    wappsto.connect();
+
+    Network *myNetwork = wappsto.createNetwork("Basic Example", "");
+    assertNotEqual(myNetwork, NULL);
+
+    DeviceDescription_t myDeviceDescription = {
+        .name = "My Test Device",
+        .product = "CreateTest",
+        .manufacturer = "",
+        .description = "description",
+        .version = "1.0",
+        .serial = "00001",
+        .protocol = "Json-RPC",
+        .communication = "WiFi",
+    };
+    Device *myDevice = myNetwork->createDevice(&myDeviceDescription);
+    assertNotEqual(myDevice, NULL);
+
+    ValueBlob_t blobParams = {  .name = "Test Blob",
+                                    .type = "test type",
+                                    .permission = READ_WRITE,
+                                    .max = 25,
+                                    .encoding = ""};
+
+
+    Value *myBlobValue = myDevice->createValueBlob(&blobParams);
+    assertNotEqual(myBlobValue, NULL);
 }
 
 void setup() {
