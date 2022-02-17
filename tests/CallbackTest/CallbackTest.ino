8543b88fb33cd763e4b9e7a0ecd29c97eaecd3fa
Karsten Tonnesen <karsten@seluxit.com>
2022-02-17 12:20:00 +0100

    Added string to callback test.

--- tests/CallbackTest/CallbackTest.ino
+++ tests/CallbackTest/CallbackTest.ino
@@ -4,7 +4,19 @@
 static bool receivedControl = false;
 static bool receivedRefresh = false;
 
-void controlLedCallback(Value *value, double data, String timestamp)
+
+void controlStringCallback(Value *value, String data, String timestamp)
+{
+    (void)value;
+    (void)timestamp;
+    if(strcmp("Correct test string", data.c_str()) == 0) {
+        receivedControl = true;
+    } else {
+        receivedControl = false;
+    }
+}
+
+void controlNumberCallback(Value *value, double data, String timestamp)
 {
     (void)value;
     (void)timestamp;
@@ -21,10 +33,12 @@ void refreshCallback(Value *value)
     receivedRefresh = true;
 }
 
-test(callbackTest) {
+test(callbackNumberTest) {
     WiFiClientSecure client;
 
     // Test setup
+    receivedRefresh = false;
+    receivedControl = false;
     client.addDeviceUuid("42b7bb41-bf32-4648-1102-aea6fca55641");
     client.addValueUuid("42b7bb41-bf32-4648-1102-aea6fca55642");
     client.addReportUuid("42b7bb41-bf32-4648-1102-aea6fca55643", "0");
@@ -54,8 +68,8 @@ test(callbackTest) {
     };
     Device *myDevice = myNetwork->createDevice(&myDeviceDescription);
 
-    ValueNumber_t myLedParameters = {   .name = "Led",
-                                        .type = "light",
+    ValueNumber_t myIntParameters = {   .name = "Int",
+                                        .type = "bool",
                                         .permission = READ_WRITE,
                                         .min = 0,
                                         .max = 1,
@@ -73,9 +87,9 @@ test(callbackTest) {
                                         .si_conversion = ""};
 
 
-    Value *myLedValue = myDevice->createValueNumber(&myLedParameters);
-    myLedValue->onControl(&controlLedCallback);
-    myLedValue->onRefresh(&refreshCallback);
+    Value *myIntValue = myDevice->createValueNumber(&myIntParameters);
+    myIntValue->onControl(&controlNumberCallback);
+    myIntValue->onRefresh(&refreshCallback);
 
     Value *myDoubleValue = myDevice->createValueNumber(&myDoubleParam);
 
@@ -97,12 +111,12 @@ test(callbackTest) {
     int testInt = 123;
 
     // Test report
-    myLedValue->report(testInt);
-    assertEqual("123", myLedValue->getReportData());
+    myIntValue->report(testInt);
+    assertEqual("123", myIntValue->getReportData());
 
     Serial.println("Report step size 1 - max .00");
-    myLedValue->report(myLatitude);
-    assertEqual("57.02", myLedValue->getReportData());
+    myIntValue->report(myLatitude);
+    assertEqual("57.02", myIntValue->getReportData());
 
     Serial.println("Report step size 0.000001");
     myDoubleValue->report(myLatitude);
@@ -110,16 +124,82 @@ test(callbackTest) {
 
 
     // Test control
-    myLedValue->control(testInt);
-    assertEqual("123", myLedValue->getControlData());
+    myIntValue->control(testInt);
+    assertEqual("123", myIntValue->getControlData());
 
     Serial.println("Control step size 1 - max .00");
-    myLedValue->control(myLatitude);
-    assertEqual("57.02", myLedValue->getControlData());
+    myIntValue->control(myLatitude);
+    assertEqual("57.02", myIntValue->getControlData());
 
     Serial.println("Control  step size 0.000001");
     myDoubleValue->control(myLatitude);
     assertEqual("57.019660", myDoubleValue->getControlData());
+}
+
+test(callbackStringTest) {
+    WiFiClientSecure client;
+
+    // Test setup
+    receivedRefresh = false;
+    receivedControl = false;
+    client.addDeviceUuid("42b7bb41-bf32-4648-1102-aea6fca55641");
+    client.addValueUuid("42b7bb41-bf32-4648-1102-aea6fca55642");
+    client.addReportUuid("42b7bb41-bf32-4648-1102-aea6fca55643", "");
+    client.addControlUuid("42b7bb41-bf32-4648-1102-aea6fca55644", "");
+
+
+    Wappsto wappsto(&client);
+    wappsto.config("4906c6be-cc7f-4c4d-8806-60a38c5fcef5", "", "", "", 0, NO_LOGS);
+
+    wappsto.connect();
+
+    Network *myNetwork = wappsto.createNetwork("Basic Example", "");
+    assertNotEqual(myNetwork, NULL);
+    DeviceDescription_t myDeviceDescription = {
+        .name = "My Device",
+        .product = "ESP32 example",
+        .manufacturer = "",
+        .description = "ESP32 Example description",
+        .version = "1.0",
+        .serial = "00001",
+        .protocol = "Json-RPC",
+        .communication = "WiFi",
+    };
+    Device *myDevice = myNetwork->createDevice(&myDeviceDescription);
+
+
+    ValueString_t stringParameters = {  .name = "String",
+                                        .type = "text",
+                                        .permission = READ_WRITE,
+                                        .max = 1};
+
+    Value *myStringValue = myDevice->createValueString(&stringParameters);
+    myStringValue->onControl(&controlStringCallback);
+    myStringValue->onRefresh(&refreshCallback);
+
+
+    // Test refresh
+    client.testRefresh("42b7bb41-bf32-4648-1102-aea6fca55643", "/network/4906c6be-cc7f-4c4d-8806-60a38c5fcef5/device/42b7bb41-bf32-4648-1102-aea6fca55641/value/42b7bb41-bf32-4648-1102-aea6fca55642/state/");
+    wappsto.dataAvailable();
+    assertEqual(receivedRefresh, true);
+
+    // Test control
+    client.testControl("42b7bb41-bf32-4648-1102-aea6fca55644", "/network/4906c6be-cc7f-4c4d-8806-60a38c5fcef5/device/42b7bb41-bf32-4648-1102-aea6fca55641/value/42b7bb41-bf32-4648-1102-aea6fca55642/state/", "Correct test string");
+    wappsto.dataAvailable();
+    assertEqual(receivedControl, true);
+
+    client.testControl("42b7bb41-bf32-4648-1102-aea6fca55644", "/network/4906c6be-cc7f-4c4d-8806-60a38c5fcef5/device/42b7bb41-bf32-4648-1102-aea6fca55641/value/42b7bb41-bf32-4648-1102-aea6fca55642/state/", "Incorrect");
+    wappsto.dataAvailable();
+    assertEqual(receivedControl, false);
+
+
+    // Test report
+    myStringValue->report("123");
+    assertEqual("123", myStringValue->getReportData());
+
+    // Test control
+    myStringValue->control("123");
+    assertEqual("123", myStringValue->getControlData());
 }
 
 void setup() {
